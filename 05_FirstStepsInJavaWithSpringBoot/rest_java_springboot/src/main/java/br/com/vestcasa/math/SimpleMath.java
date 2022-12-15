06d4fd05e7a9d8e77d08dbfc06950887164517a6
GUILHERME FERREIRA DA SILVA <guilherme.ferreira@grupovestcasa.local>
2022-12-15 17:42:27 -0300

    refactor: classes separadas para os metodos de conversão e operações matematicas basicas

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest_java_springboot/src/main/java/br/com/vestcasa/math/SimpleMath.java
@@ -0,0 +1,28 @@
+package br.com.vestcasa.math;
+
+public class SimpleMath {
+
+	public Double sum(Double numberOne, Double numberTwo) {		
+		return numberOne + numberTwo;
+	}
+	
+	public Double sub(Double numberOne, Double numberTwo) {		
+		return numberOne - numberTwo;
+	}
+	
+	public Double multi(Double numberOne, Double numberTwo) {
+		return numberOne * numberTwo;
+	}
+	
+	public Double division(Double numberOne, Double numberTwo) {		
+		return numberOne / numberTwo;
+	}
+	
+	public Double mean(Double numberOne, Double numberTwo) {		
+		return (numberOne + numberTwo) / 2;
+	}
+	
+	public Double squareRoot(Double number) {		
+		return Math.sqrt(number);
+	}
+}
