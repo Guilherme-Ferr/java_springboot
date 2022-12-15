8616e8975998c224c7be537c343245ce3a127613
GUILHERME FERREIRA DA SILVA <guilherme.ferreira@grupovestcasa.local>
2022-12-15 18:07:32 -0300

    feat: dando inicio a person controller e person services

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest_java_springboot/src/main/java/br/com/vestcasa/controllers/PersonController.java
@@ -0,0 +1,25 @@
+package br.com.vestcasa.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.vestcasa.converters.NumberConverter;
+import br.com.vestcasa.exceptions.UnsupportedMathOperationException;
+import br.com.vestcasa.math.SimpleMath;
+
+@RestController
+public class PersonController {
+		
+	@GetMapping(value = "/sum/{numberOne}/{numberTwo}")
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+		) throws Exception {
+		
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		
+		return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+}
---
+++ 05_FirstStepsInJavaWithSpringBoot/rest_java_springboot/src/main/java/br/com/vestcasa/services/PersonServices.java
@@ -0,0 +1,10 @@
+package br.com.vestcasa.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+public class PersonServices {
+
+	private final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(null)
+}
